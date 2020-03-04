97afef2e01067a1d90525a5c48403fbb0bdab8b1
Luiz Otávio <luizomf@gmail.com>
2020-03-04 04:31:24 -0300

    Monostate

--- singleton/monostate_1.py
+++ singleton/monostate_1.py
@@ -3,23 +3,25 @@ Monostate (ou Borg) - É uma variação do Singleton proposto
 por Alex Martelli que tem a intenção de garantir que o
 estado do objeto seja igual para todas as instâncias.
 """
+from __future__ import annotations
+from typing import Dict
 
 
 class StringReprMixin:
-    def __str__(self):
+    def __str__(self) -> str:
         params = ', '.join(
             [f'{k}={v}' for k, v in self.__dict__.items()]
         )
         return f'{self.__class__.__name__}({params})'
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.__str__()
 
 
 class MonoStateSimple(StringReprMixin):
-    _state: dict = {}
+    _state: Dict = {}
 
-    def __init__(self, nome=None, sobrenome=None):
+    def __init__(self, nome=None, sobrenome=None) -> None:
         self.__dict__ = self._state
 
         if nome is not None:
