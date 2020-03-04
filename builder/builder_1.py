63271c3e208ec49dedce121a3b482de973f2ef53
Luiz Otávio <luizomf@gmail.com>
2020-03-04 04:23:12 -0300

    Builder

--- builder/builder_1.py
+++ builder/builder_1.py
@@ -10,95 +10,97 @@ e isso já é possível no Python sem este padrão.
 Geralmente o builder aceita o encadeamento de métodos
 (method chaining).
 """
+from __future__ import annotations
 from abc import ABC, abstractmethod
+from typing import List
 
 
 class StringReprMixin:
-    def __str__(self):
+    def __str__(self) -> str:
         params = ', '.join(
             [f'{k}={v}' for k, v in self.__dict__.items()]
         )
         return f'{self.__class__.__name__}({params})'
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.__str__()
 
 
 class User(StringReprMixin):
-    def __init__(self):
+    def __init__(self) -> None:
         self.firstname = None
         self.lastname = None
         self.age = None
-        self.phone_numbers = []
-        self.addresses = []
+        self.phone_numbers: List = []
+        self.addresses: List = []
 
 
 class IUserBuilder(ABC):
     @property
     @abstractmethod
-    def result(self): pass
+    def result(self) -> User: pass
 
     @abstractmethod
-    def add_firstname(self, firstname): pass
+    def add_firstname(self, firstname) -> UserBuilder: pass
 
     @abstractmethod
-    def add_lastname(self, lastname): pass
+    def add_lastname(self, lastname) -> UserBuilder: pass
 
     @abstractmethod
-    def add_age(self, age): pass
+    def add_age(self, age) -> UserBuilder: pass
 
     @abstractmethod
-    def add_phone(self, phone): pass
+    def add_phone(self, phone) -> UserBuilder: pass
 
     @abstractmethod
-    def add_address(self, address): pass
+    def add_address(self, address) -> UserBuilder: pass
 
 
 class UserBuilder(IUserBuilder):
-    def __init__(self):
+    def __init__(self) -> None:
         self.reset()
 
-    def reset(self):
+    def reset(self) -> None:
         self._result = User()
 
     @property
-    def result(self):
+    def result(self) -> User:
         return_data = self._result
         self.reset()
         return return_data
 
-    def add_firstname(self, firstname):
+    def add_firstname(self, firstname) -> UserBuilder:
         self._result.firstname = firstname
         return self
 
-    def add_lastname(self, lastname):
+    def add_lastname(self, lastname) -> UserBuilder:
         self._result.lastname = lastname
         return self
 
-    def add_age(self, age):
+    def add_age(self, age) -> UserBuilder:
         self._result.age = age
         return self
 
-    def add_phone(self, phone):
+    def add_phone(self, phone) -> UserBuilder:
         self._result.phone_numbers.append(phone)
         return self
 
-    def add_address(self, address):
+    def add_address(self, address) -> UserBuilder:
         self._result.addresses.append(address)
         return self
 
 
 class UserDirector:
-    def __init__(self, builder: UserBuilder):
+    def __init__(self, builder: UserBuilder) -> None:
         self._builder = builder
 
-    def with_age(self, firstname, lastname, age):
+    def with_age(self, firstname, lastname, age) -> User:
         self._builder.add_firstname(firstname)\
             .add_lastname(lastname)\
             .add_age(age)
         return self._builder.result
 
-    def with_address(self, firstname, lastname, address):
+    def with_address(self, firstname, lastname, address) -> User:
         self._builder.add_firstname(firstname)\
             .add_lastname(lastname)\
             .add_address(address)
