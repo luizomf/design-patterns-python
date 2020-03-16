9c1f860b7305c41b43b92cccb5ed49547f294d9c
Luiz Otávio <luizomf@gmail.com>
2020-03-15 21:20:32 -0300

    Façade

--- structural/facade/facade_1.py
+++ structural/facade/facade_1.py
@@ -1,8 +1,8 @@
 """
 Façade (Fachada) é um padrão de projeto estrutural
-que tem a intenção de forcener uma interface
+que tem a intenção de fornecer uma interface
 unificada para um conjunto de interfaces em um
 subsistema. Façade define uma interface de nível
-mais alto que torna o subsistema mais fácil de
-ser usado.
+mais alto que torna o subsistema mais fácil de ser
+usado.
 """
