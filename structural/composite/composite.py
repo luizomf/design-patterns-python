2eb67f23d0b6ab09a65cc49df63bf01a5d5e05e1
Luiz Otávio <luizomf@gmail.com>
2020-03-23 10:43:47 -0300

    Composite

--- structural/composite/composite.py
+++ structural/composite/composite.py
@@ -19,3 +19,86 @@ Objetos Leaf são objetos simples, da ponta e sem filhos.
 Geralmente, são esses objetos que realizam o trabalho
 real da aplicação.
 """
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class BoxStructure(ABC):
+    """ Component """
+    @abstractmethod
+    def print_content(self) -> None: pass
+
+    @abstractmethod
+    def get_price(self) -> float: pass
+
+    def add(self, child: BoxStructure) -> None: pass
+    def remove(self, child: BoxStructure) -> None: pass
+
+
+class Box(BoxStructure):
+    """ Composite """
+
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self._children: List[BoxStructure] = []
+
+    def print_content(self) -> None:
+        print(f'\n{self.name}:')
+        for child in self._children:
+            child.print_content()
+
+    def get_price(self) -> float:
+        return sum([
+            child.get_price() for child in self._children
+        ])
+
+    def add(self, child: BoxStructure) -> None:
+        self._children.append(child)
+
+    def remove(self, child: BoxStructure) -> None:
+        if child in self._children:
+            self._children.remove(child)
+
+
+class Product(BoxStructure):
+    """ Leaf """
+
+    def __init__(self, name: str, price: float) -> None:
+        self.name = name
+        self.price = price
+
+    def print_content(self) -> None:
+        print(self.name, self.price)
+
+    def get_price(self) -> float:
+        return self.price
+
+
+if __name__ == "__main__":
+    # Leaf
+    camiseta1 = Product('camiseta1', 10)
+    camiseta2 = Product('camiseta2', 10)
+    camiseta3 = Product('camiseta3', 10)
+
+    # Composite
+    caixa_camisetas = Box('Caixa de camiseta')
+    caixa_camisetas.add(camiseta1)
+    caixa_camisetas.add(camiseta2)
+    caixa_camisetas.add(camiseta3)
+
+    # Leaf
+    smartphone1 = Product('smartphone1', 10000)
+    smartphone2 = Product('smartphone2', 10000)
+
+    # Composite
+    caixa_smartphones = Box('Caixa de Smartphones')
+    caixa_smartphones.add(smartphone1)
+    caixa_smartphones.add(smartphone2)
+
+    # Composite
+    caixa_grande = Box('Caixa grande')
+    caixa_grande.add(caixa_camisetas)
+    caixa_grande.add(caixa_smartphones)
+    caixa_grande.print_content()
+    print(caixa_grande.get_price())
