15c8475dadc939119c1adb8640d9f85f2aa6685d
Luiz Otávio <luizomf@gmail.com>
2020-03-25 00:54:57 -0300

    Decorator

---
+++ structural/decorator/decorator.py
@@ -0,0 +1,15 @@
+"""
+Decorator é um padrão de projeto estrutural que permite que você
+adicione novos comportamentos em objetos ao colocá-los dentro de
+um "wrapper" (decorador) de objetos.
+Decoradores fornecem uma alternativa flexível ao uso de subclasses
+para a extensão de funcionalidades.
+
+Decorator (padrão de projeto) != Decorator em Python
+
+Python decorator -> Um decorator é um callable que aceita outra
+função como argumento (a função decorada). O decorator pode
+realizar algum processamento com a função decorada e devolvê-la
+ou substituí-la por outra função ou objeto invocável.
+Do livro "Python Fluente", por Luciano Ramalho (pág. 223)
+"""
